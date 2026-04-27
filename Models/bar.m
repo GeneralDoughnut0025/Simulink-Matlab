0bab06ce418668e1892a6672eca8276846253d77
LEONEL REAGAN REBELLO <162774869+GeneralDoughnut0025@users.noreply.github.com>
2026-04-27 14:30:15 +0200

    Add files via upload

---
+++ Models/bar.m
@@ -0,0 +1,5 @@
+x=[12, 15, 13, 11, 14, 17];
+barh(x,0.5, 'r','EdgeColor','k','LineWidth',2);
+title('Bar graph','FontSize',16);
+xlabel('Amplitude', 'FontSize',10);
+ylabel('Number', 'FontSize',10);
